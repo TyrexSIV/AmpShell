fa23a1b34fc4111746f98be8fe33a7223bd4088d
Maximilien Noal <noal.maximilien@gmail.com>
2019-07-14 20:24:15 +0200

    PathToBitmapConverter

---
+++ Converters/PathToBitmapConverter.cs
@@ -0,0 +1,23 @@
+﻿using Avalonia.Data.Converters;
+using Avalonia.Media.Imaging;
+
+using System;
+using System.Globalization;
+
+namespace AmpShell.Converters
+{
+    public class PathToBitmapConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string path = (string)value;
+
+            return new Bitmap(path);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
