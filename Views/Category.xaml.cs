5933085a3eda1c295f186252fa25379ff25e0083
Maximilien Noal <noal.maximilien@gmail.com>
2019-10-12 16:52:05 +0200

    Code-behind for all windows
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>

---
+++ Views/Category.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace AmpShell.Views
+{
+    public class Category : Window
+    {
+        public Category()
+        {
+            this.InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
---
+++ Views/Game.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace AmpShell.Views
+{
+    public class Game : Window
+    {
+        public Game()
+        {
+            InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
